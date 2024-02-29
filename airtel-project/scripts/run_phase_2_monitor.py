40dee189335c997095e682737c1e02ee30928dfe
AFodor <alexandrueduard.fodor@spirent.com>
2024-02-29 16:03:40 +0200

    Debug steps for params

--- airtel-project/scripts/run_phase_2_monitor.py
+++ airtel-project/scripts/run_phase_2_monitor.py
@@ -67,6 +67,15 @@ def main():
         else:
             log_worker.warning(f"Argument {sys.argv[i]} is not recognized and will not be used.")
 
+    print('jira_project_key: ', jira_project_key)
+    print('jira_project_release_name: ', jira_project_release_name)
+    print('zephyr_test_cycle_name: ', zephyr_test_cycle_name)
+    print('story_key_for_comment: ', story_key_for_comment)
+    print('zephyr_build: ', zephyr_build)
+    print('runlist_name: ', runlist_name)
+    print('topology_name: ', topology_name)
+    print('zephyr_test_cycle_id: ', zephyr_test_cycle_id)
+
     if jira_project_key == "":
         log_worker.error(f"Argument jira_project_key is empty, exiting execution.")
         log_worker.error(f"Finished: FAILED")
